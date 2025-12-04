f105bc3aec45f33184c0bdc85aa1e66bcf3f247f
Niranjan Nadiu <niranjannaidu789@gmail.com>
2025-12-04 21:15:55 +0530

    trying agian

--- qualisense_project/settings.py
+++ qualisense_project/settings.py
@@ -117,7 +117,13 @@ USE_TZ = True
 # -----------------------------------------
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "quali_defect_app/static"),
+]
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = "/media/"
